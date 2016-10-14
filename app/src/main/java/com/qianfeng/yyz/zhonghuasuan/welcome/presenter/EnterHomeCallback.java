5c14d086bade752c4bf5eb19651ee74933a05eb5
yzz <1316234380@qq.com>
2016-10-14 22:56:06 +0800

    4

---
+++ app/src/main/java/com/qianfeng/yyz/zhonghuasuan/welcome/presenter/EnterHomeCallback.java
@@ -0,0 +1,11 @@
+package com.qianfeng.yyz.zhonghuasuan.welcome.presenter;
+
+import android.content.Context;
+
+/**
+ * Created by Administrator on 2016/10/14 0014.
+ */
+public interface EnterHomeCallback {
+
+    void enterHome(Context context);
+}
