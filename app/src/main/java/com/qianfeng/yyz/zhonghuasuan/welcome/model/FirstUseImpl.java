3d1ad4326c639b338208ddb25620efa2bddcc012
yzz <1316234380@qq.com>
2016-10-14 17:22:23 +0800

    first

---
+++ app/src/main/java/com/qianfeng/yyz/zhonghuasuan/welcome/model/FirstUseImpl.java
@@ -0,0 +1,11 @@
+package com.qianfeng.yyz.zhonghuasuan.welcome.model;
+
+/**
+ * Created by Administrator on 2016/10/14 0014.
+ */
+public class FirstUseImpl  implements IFirstUse{
+    @Override
+    public boolean check() {
+        return false;
+    }
+}
